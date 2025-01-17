0df9ee3a1f3f45a705dadf0a75c55664f9a751c3
Raushan Pandey <raushan.rkp1@gmail.com>
2025-01-17 19:00:05 +0530

    upload size increased

--- BE/src/middleware/uploadMiddleware.ts
+++ BE/src/middleware/uploadMiddleware.ts
@@ -2,7 +2,7 @@ import multer from 'multer';
 
 const upload = multer({
     storage: multer.memoryStorage(),
-    limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB
+    limits: { fileSize: 300 * 1024 * 1024 }, // 300 MB
     fileFilter: (req, file, cb) => {
         if (file.mimetype === 'application/vnd.android.package-archive') {
             cb(null, true);
